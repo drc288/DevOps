f797dd15864e58c2442e8da2cdb43397fb74c3b4
David Rosero C <davidroserocalle@gmail.com>
2020-01-09 01:29:35 -0500

    create the file to get the configuretion for master and angent puppet

---
+++ service_puppet.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# This scrip install a puppet clien or master server, using the options 1 or 2
+# ############################################################################
+# #                           Puppet installer                               #
+# ############################################################################
+#
+# Create the configuratioen, get argv to get the installer
+if [ "$#" -eq 1 ]; then
+  if [ `whoami` != root ]; then
+    echo "Please run this script as root or using sudo"
+    exit
+  else
+    if [ "${1^^}" == "AGENT" ]; then
+      echo "yes agent"
+    elif [ "${1^^}" == "MASTER" ]; then
+      echo "yes master"
+    else
+      echo "The option '$1' is no recogniced"
+    fi
+  fi
+else
+  echo "Usage:"
+  echo "  ./service_puppet.sh agent to create a client"
+  echo "  ./service_puppet.sh master to create a server"
+fi
