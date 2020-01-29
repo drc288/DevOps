fddd7154ee348d18af6a7318d1aeafc71acce3b5
David Rosero C <davidroserocalle@gmail.com>
2020-01-29 00:19:45 -0500

    update file, remove the try and the except, and add variable to set the hostname

--- fabric_files/install_nginx.py
+++ fabric_files/install_nginx.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
 from fabric.api import env, run, sudo
-from sys import argv
+import socket
 env.hosts = ["192.168.33.10", "192.168.33.11"]
-
 
 def install_nginx():
     """
     install_nginx - update the server and install nginx
     and add a template
     """
-    try:
-        run("sudo apt-get update")
-        run("sudo apt-get -y install nginx")
-        run("sudo service nginx start")
-        run("sudo mkdir -p /var/www/html")
-        run("sudo touch /var/www/html/index.html")
-        sudo("sed -i 's|root /usr/share/nginx/html;|root /var/www/html;|g' /etc/nginx/sites-available/default")
-        sudo("echo 'Holberton School' > /var/www/html/index.html")
-        run("sudo service nginx restart")
-    except Exception:
-        pass
+    data = run("echo $HOSTNAME")
+    run("sudo apt-get update")
+    run("sudo apt-get -y install nginx")
+    run("sudo service nginx start")
+    run("sudo mkdir -p /var/www/html")
+    run("sudo touch /var/www/html/index.html")
+    sudo("echo 'Holberton School' > /var/www/html/index.html")
+    sudo("sed -i 's|root /usr/share/nginx/html;|root /var/www/html;|g' /etc/nginx/sites-available/default")
+    sudo('sed -i "s|ipv6only=on;|ipv6only=on;\\n\\tadd_header X-Server-By {};|g" /etc/nginx/sites-available/default'.format(data))
+    run("sudo service nginx restart")
